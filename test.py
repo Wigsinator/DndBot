1e45adc3b390cceb99f96cecce7c8ca228e6dcc7
Wigsinator <wigsinator@gmail.com>
2017-02-11 00:32:11 -0500

    Baseline work

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+import xml.etree.ElementTree as ET
 
 client = discord.Client()
 
@@ -24,4 +25,33 @@ async def on_message(message):
 		await asyncio.sleep(5)
 		await client.send_message(message.channel, 'Done sleeping')
 
-client.run('Mjc5Njk0NjEwMTc4MTEzNTM4.C3-lkA.P5lIVnCvAihReW14Z8j9ow6i9Bk')
+	elif message.content.startswith('*class'):
+		tree = ET.parse('XMLs/Character Files/Classes 2.5.xml')
+		root = tree.getroot()
+
+		search = 'Barbarian'
+		level = '1'
+		abilities = []
+
+
+		for clas in root.findall('class'):
+			name = clas.get('name')
+			if search == name:
+				#print(name)
+				for auto in clas.findall('autolevel'):
+					lev = auto.get('level')
+					if lev == level:
+						#print('Level', lev, 'ability')
+						for feature in auto.findall('feature'):
+							ability = feature.get('name')
+							#print(ability)
+							abilities.append(ability)
+						break
+				break
+		
+		#print('```', name, lev, *abilities, '```', sep = '\n')
+		output = '```' +'\n'+ name +'\n'+ lev +'\n'+ '\n'.join(abilities) +'\n'+ '```'
+		await client.send_message(message.channel, output)
+
+
+client.run('Mjc5NzA3MTI4NDQ3MzY5MjI1.C4AuMg.VQFvlSGKVQq_2HTNZGTeN0K_HTY')
